9eabb00dbfaf288cba606992893444fb88c61800
Łukasz Marcinkiewicz <marcinkiewi_cz@poczta.fm>
2023-11-28 17:45:03 +0100

    package.cpp z implementacją kontruktora Package(&&) i operatora.

--- src/package.cpp
+++ src/package.cpp
@@ -1,3 +1,19 @@
 //
 // Created by mmich on 28.11.2023.
 //
+
+#include "package.hpp"
+
+Package::Package(Package && p){
+    id_ = p.id_;
+    p.id_ = -1;
+}
+
+Package & Package::operator=(Package && p){
+    if(id_ != -1){
+        freed_IDs.insert(id_);
+        assigned_IDs.erase(id_);
+    }
+    id_ = p.id_;
+    p.id_ = -1;
+}
