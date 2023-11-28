581e22b66a6affd3010b96a84ab1e5d67784ee40
Łukasz Marcinkiewicz <marcinkiewi_cz@poczta.fm>
2023-11-28 17:30:42 +0100

    package.hpp ze wstępnymi deklaracjami.

--- include/package.hpp
+++ include/package.hpp
@@ -5,4 +5,26 @@
 #ifndef IMPLEMENTATION_NETSIM_PACKAGE_HPP
 #define IMPLEMENTATION_NETSIM_PACKAGE_HPP
 
+#include <ostream>
+#include "types.hpp"
+
+class Package{
+public:
+    Package();
+
+    Package(ElementID);
+
+    Package(Package&&);
+
+    Package & operator = (Package &&);
+
+    ElementID get_id() const;
+
+    ~Package();
+private:
+    ElementID id_;
+//    static std::set<ElementID> assigned_IDs;
+
+};
+
 #endif //IMPLEMENTATION_NETSIM_PACKAGE_HPP
