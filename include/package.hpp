2966b99fafccbae2db509f5e0c3c3ac742f47942
Marek Michelis <mmichelis@student.agh.edu.pl>
2023-12-04 23:21:45 +0100

    remove bug in package deconstructor & add list initialization.
    %%PackageTest & PackageQueueTest passed

--- include/package.hpp
+++ include/package.hpp
@@ -13,20 +13,21 @@ class Package{
 public:
     Package();
 
-    Package(ElementID id) : id_(id){}
+    Package(ElementID id) : id_(id) {}
 
-    Package & operator = (Package &&);
+    Package & operator = (Package &&) noexcept;
 
-    Package(Package&&);
+    Package(Package&&) noexcept;
 
     ElementID get_id() const {return id_;};
 
     ~Package();
 private:
     ElementID id_;
-    inline static std::set<ElementID> assigned_IDs;
-    inline static std::set<ElementID> freed_IDs;
+    static std::set<ElementID> assigned_IDs;
+    static std::set<ElementID> freed_IDs;
 
 };
+
 
 #endif //IMPLEMENTATION_NETSIM_PACKAGE_HPP
--- src/package.cpp
+++ src/package.cpp
@@ -1,5 +1,8 @@
 #include "package.hpp"
 
+std::set<ElementID> Package::assigned_IDs = {};
+std::set<ElementID> Package::freed_IDs = {};
+
 Package::Package() {
     if (freed_IDs.empty() && assigned_IDs.empty()) {
         id_ = 1;
@@ -13,24 +16,33 @@ Package::Package() {
     assigned_IDs.insert(id_);
 }
 
-Package & Package::operator=(Package && p){
+
+Package & Package::operator=(Package && p) noexcept {
+    if (this == &p) return *this;
+
     if(id_ != -1){
-        freed_IDs.insert(id_);
-        assigned_IDs.erase(id_);
+        freed_IDs.insert(this->id_);
+        assigned_IDs.erase(this->id_);
     }
-    id_ = p.id_;
+
+    this->id_ = p.id_;
+    assigned_IDs.insert(this->id_);
+
     p.id_ = -1;
+
     return *this;
+
 }
 
-Package::Package(Package && p){
+Package::Package(Package && p) noexcept{
     id_ = p.id_;
     p.id_ = -1;
 }
 
 Package::~Package(){
-    if(id_==-1){
+    if(id_!=-1){
         assigned_IDs.erase(id_);
         freed_IDs.insert(id_);
     }
+
 }
