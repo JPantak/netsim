446203548641d6bdfe6c91179ab166db18990e33
Łukasz Marcinkiewicz <marcinkiewi_cz@poczta.fm>
2023-12-05 17:10:02 +0100

    Dodanie wstępnej implementacji klasy Ramp w nodes.hpp

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -5,4 +5,20 @@
 #ifndef IMPLEMENTATION_NETSIM_NODES_HPP
 #define IMPLEMENTATION_NETSIM_NODES_HPP
 
+#include "types.hpp"
+
+class Ramp{
+public:
+    Ramp(ElementID id, TimeOffset di) : id_(id), di_(di) {};
+
+    void deliver_goods(Time t);
+
+    TimeOffset get_delivery_interval(void) const {return di_;};
+
+    ElementID get_id(void) const {return id_;}
+private:
+    ElementID id_;
+    TimeOffset di_;
+};
+
 #endif //IMPLEMENTATION_NETSIM_NODES_HPP
