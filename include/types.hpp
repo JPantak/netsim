fabe516770705979d9dbf5ef17c44862efbc9520
Łukasz Marcinkiewicz <marcinkiewi_cz@poczta.fm>
2023-12-05 17:39:44 +0100

    Dodanie aliasu ProbabilityGenerator do types.hpp

--- include/types.hpp
+++ include/types.hpp
@@ -5,12 +5,14 @@
 #ifndef IMPLEMENTATION_NETSIM_TYPES_HPP
 #define IMPLEMENTATION_NETSIM_TYPES_HPP
 
+#include <functional>
+
 using ElementID = int;
 
 using Time = unsigned int;
 
 using TimeOffset = unsigned int;
 
-using ProbabilityGenerator = double;
+using ProbabilityGenerator = std::function<double(void)>;
 
 #endif //IMPLEMENTATION_NETSIM_TYPES_HPP
