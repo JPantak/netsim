61da999b0b54c3baba437e6b98d2dd00f861309e
JPantak <62845142+JPantak@users.noreply.github.com>
2023-12-05 17:16:58 +0100

    update helpers

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -4,5 +4,6 @@
 
 #ifndef IMPLEMENTATION_NETSIM_HELPERS_HPP
 #define IMPLEMENTATION_NETSIM_HELPERS_HPP
-
+double probability_generator_1();
+double probability_generator_2();
 #endif //IMPLEMENTATION_NETSIM_HELPERS_HPP
--- src/helpers.cpp
+++ src/helpers.cpp
@@ -1,3 +1,16 @@
-//
-// Created by mmich on 28.11.2023.
-//
+#include <cstdlib>
+#include <iostream>
+#include <random>
+#include <ctime>
+ 
+std::random_device rd;
+std::mt19937 rng(rd());
+ 
+double probability_generator_1() {
+    return std::generate_canonical<double, 10>(rng);
+}
+ 
+double probability_generator_2() {
+    return (double) (rand() % RAND_MAX) / RAND_MAX;  // NIEZALECANE: mała losowość generowanego ciągu liczb...
+}
+ 
