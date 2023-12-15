43263a8366a18badf00594dd04119f22e0ab601f
JPantak <62845142+JPantak@users.noreply.github.com>
2023-12-15 10:46:32 +0100

    Update factory.hpp

--- include/factory.hpp
+++ include/factory.hpp
@@ -40,23 +40,29 @@ private:
 
 class Factory{
 public:
-    void add_ramp(Ramp&&);
-    void remove_ramp(ElementID id);
+    void add_ramp(Ramp&& ramp) {Ramps.add(std::move(ramp));}
+    void remove_ramp(ElementID id) {Ramps.remove_by_id(id);}
 
-    NodeCollection<Ramp>::iterator find_ramp_by_id(ElementID id);
-    NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id) const;
-    NodeCollection<Ramp>::const_iterator ramp_cbegin();
-    NodeCollection<Ramp>::const_iterator ramp_cend();
+    void add_worker(Worker&& worker) {Workers.add(std::move(worker));}
+    void remove_worker(ElementID id) {Workers.remove_by_id(id);}
 
-    NodeCollection<Worker>::iterator find_worker_by_id(ElementID id);
-    NodeCollection<Worker>::const_iterator find_worker_by_id(ElementID id) const;
-    NodeCollection<Worker>::const_iterator worker_cbegin();
-    NodeCollection<Worker>::const_iterator worker_cend();
+    void add_storehouse(Storehouse&& storehouse){Storehouses.add(std::move(storehouse));}
+    void remove_storehouse(ElementID id){Storehouses.remove_by_id(id);}
 
-    NodeCollection<Storehouse>::iterator find_storehouse_by_id(ElementID id);
-    NodeCollection<Storehouse>::const_iterator find_storehouse_by_id(ElementID id) const;
-    NodeCollection<Storehouse>::const_iterator storehouse_cbegin();
-    NodeCollection<Storehouse>::const_iterator storehouse_cend();
+    NodeCollection<Ramp>::iterator find_ramp_by_id(ElementID id) {return Ramps.find_by_id(id);}
+    NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id) const {return Ramps.find_by_id(id);}
+    NodeCollection<Ramp>::const_iterator ramp_cbegin() {return Ramps.cbegin();}
+    NodeCollection<Ramp>::const_iterator ramp_cend() {return Ramps.end();}
+
+    NodeCollection<Worker>::iterator find_worker_by_id(ElementID id){return Workers.find_by_id(id);}
+    NodeCollection<Worker>::const_iterator find_worker_by_id(ElementID id) const {return Workers.find_by_id(id);}
+    NodeCollection<Worker>::const_iterator worker_cbegin() {return Workers.cbegin();}
+    NodeCollection<Worker>::const_iterator worker_cend() {return Workers.cend();}
+
+    NodeCollection<Storehouse>::iterator find_storehouse_by_id(ElementID id){return Storehouses.find_by_id(id);}
+    NodeCollection<Storehouse>::const_iterator find_storehouse_by_id(ElementID id) const {return Storehouses.find_by_id(id);}
+    NodeCollection<Storehouse>::const_iterator storehouse_cbegin() {return Storehouses.cbegin();}
+    NodeCollection<Storehouse>::const_iterator storehouse_cend() {return Storehouses.cend();}
     
     void remove_receiver(NodeCollection<Ramp>& collection, ElementID id);
     void remove_receiver(NodeCollection<Worker>& collection, ElementID id);
@@ -66,6 +72,11 @@ public:
     void do_delivieries(Time t);
     void do_package_passing(void);
     void do_work(Time t);
+
+private:
+    NodeCollection<Ramp> Ramps;
+    NodeCollection<Worker> Workers;
+    NodeCollection<Storehouse> Storehouses;
 };
 
 #endif
