ec4f75dbe7386aa54d2b4237fa659a13f8d75c09
Marek Michelis <mmichelis@student.agh.edu.pl>
2024-01-01 17:23:25 +0100

    add simulation function & simulation unit test

--- include/simulation.hpp
+++ include/simulation.hpp
@@ -1,8 +1,11 @@
-//
-// Created by mmich on 28.11.2023.
-//
-
 #ifndef IMPLEMENTATION_NETSIM_SIMULATION_HPP
 #define IMPLEMENTATION_NETSIM_SIMULATION_HPP
+
+#include "factory.hpp"
+#include <functional>
+#include <system_error>
+#include "reports.hpp"
+
+void simulate(Factory &f, TimeOffset d, std::function<void (Factory &, Time)> rf);
 
 #endif //IMPLEMENTATION_NETSIM_SIMULATION_HPP
--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,3 +1,14 @@
-//
-// Created by mmich on 28.11.2023.
-//
+#include "simulation.hpp"
+
+void simulate(Factory &f, TimeOffset d, std::function<void (Factory &, Time)> rf) {
+    if (!f.is_consistent()) {
+        throw std::logic_error("Factory network is inconsistent!!!");
+    }
+
+    for (Time current_simulation_time = 0; current_simulation_time < d; ++current_simulation_time) {
+        f.do_delivieries(current_simulation_time);
+        f.do_package_passing();
+        f.do_work(current_simulation_time);
+        rf(f, current_simulation_time);
+    }
+}
